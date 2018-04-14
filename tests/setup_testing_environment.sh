0edb8051966c3ae93918f3e569336d54d89718d6
Ori Hoch <ori@uumpa.com>
2018-04-14 11:12:29 +0300

    upgrade to latest kubectl - to match minikube release

--- tests/setup_testing_environment.sh
+++ tests/setup_testing_environment.sh
@@ -23,7 +23,7 @@ EOF
 
 mkdir -p /tmp/build_nsenter && pushd /tmp/build_nsenter && check_or_build_nsenter && sudo chmod +x ./nsenter && sudo mv ./nsenter /usr/local/bin/ && popd &&\
 which nsenter &&\
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.4/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ &&\
 sudo -E minikube start --vm-driver=none &&\
 minikube update-context &&\
