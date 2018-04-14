7db8457d40e8a70e4c3ed074c22680a5a69a40f7
Ori Hoch <ori@uumpa.com>
2018-04-14 11:24:19 +0300

    update kubectl / minikube versions

--- tests/setup_testing_environment.sh
+++ tests/setup_testing_environment.sh
@@ -23,9 +23,9 @@ EOF
 
 mkdir -p /tmp/build_nsenter && pushd /tmp/build_nsenter && check_or_build_nsenter && sudo chmod +x ./nsenter && sudo mv ./nsenter /usr/local/bin/ && popd &&\
 which nsenter &&\
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ &&\
-sudo -E minikube start --vm-driver=none &&\
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.4/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ &&\
+sudo -E minikube start --vm-driver=none --kubernetes-version=v1.9.4 &&\
 minikube update-context &&\
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done &&\
 kubectl get nodes &&\
