3e8f2d2f0b82ab60684d0b0e8081c4251c6d7452
Mickey Rouash <mickey.rouash@gmail.com>
2018-09-13 15:37:40 +0300

    update on the fly kube/config to docker image root

--- tests/minikube_docker_build.sh
+++ tests/minikube_docker_build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 mkdir .kube .minikube
-cp ~/.kube/config .kube/ && cp ~/.minikube/client.*   .minikube/ && cp ~/.minikube/ca.crt .minikube/
+#copy the current user minkube configuration to docker  image
+cp ~/.minikube/client.*   .minikube/ && cp ~/.minikube/ca.crt .minikube/
+# modify cthe config file from the current user to point to root user on the docker image
+sed -e 's|\(.*client-key:\).*\(client\.key$\)|\1 /root/\2|'\
+        -e 's|\(.*client-certificate:\).*\(client\.crt$\)|\1 /root/\2|' \
+        -e   's|\(.*certificate-authority:\).*\(ca\.crt$\)|\1 /root/\2|' ~/.kube/config >  .kube/config
 minikube mount `pwd`:/kubernetes-db-to-s3 &
 sleep 2
 echo 'cd /kubernetes-db-to-s3; docker build  -t db-backup .; exit' | minikube ssh &&\
@@ -9,3 +14,4 @@ kill %1 && sleep 1
 [ "${?}" != "0" ] && echo failed to build the docker image inside minikube && exit 1
 echo db-backup docker image was built successfully inside minikube
 exit 0
+ 
